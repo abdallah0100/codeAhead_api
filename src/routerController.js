01e7a0fd840d03e0c3f99325325e695638adad34
Abdallah <abdlaa1912@gmail.com>
2022-12-21 18:23:02 +0200

    added login request handling and modified responses

--- src/routerController.js
+++ src/routerController.js
@@ -2,29 +2,27 @@ const httpStatus = require("http-status");
 const userHandlers = require('./user/userHandlers');
 const catchAsync = require("./utils/catchAsync");
 
-// const handleRegister = catchAsync( async (req, res))=>{
-//     console.log(userHandlers.userExist(req.body));
-//     if (userHandlers.userExist(req.body) > 0) res.status(httpStatus.BAD_REQUEST).send({statusCode: httpStatus.BAD_REQUEST, msg: "* Username has already been taken"});
-//     else if (userHandlers.emailExist(req.body) > 0) res.status(httpStatus.BAD_REQUEST).send({statusCode: httpStatus.BAD_REQUEST, msg: "* Email has already been taken"});
-//     else if (userHandlers.register(req.body) > 0) res.status(httpStatus.OK).send({ statusCode: httpStatus.ok, msg: "Account created" })
-//     else res.status(httpStatus.BAD_REQUEST).send({statusCode: httpStatus.BAD_REQUEST, msg: "* An error occured *"});
-// };
-
 const handleRegister = catchAsync(async (req, res)=>{
     const userexists = await userHandlers.userExist(req.body);
     const emailexists = await userHandlers.emailExist(req.body);
     if (userexists > 0)
-        res.status(httpStatus.BAD_REQUEST).send({statusCode: httpStatus.BAD_REQUEST, msg: "* Username has already been taken"});
+        res.status(httpStatus.BAD_REQUEST).send({ msg: "* Username has already been taken"});
     else if (emailexists > 0)
-        res.status(httpStatus.BAD_REQUEST).send({statusCode: httpStatus.BAD_REQUEST, msg: "* Email has already been taken"});
+        res.status(httpStatus.BAD_REQUEST).send({ msg: "* Email has already been taken"});
     else{
         const addUser = await userHandlers.register(req.body);
         if (addUser > 0)
-            res.status(httpStatus.OK).send({ statusCode: httpStatus.ok, msg: "Account created" })
+            res.status(httpStatus.OK).send({ msg: "Account created" })
         else
-            res.status(httpStatus.BAD_REQUEST).send({statusCode: httpStatus.BAD_REQUEST, msg: "* An error occured *"});
+            res.status(httpStatus.BAD_REQUEST).send({ msg: "* An error occured *"});
     }
 });
 
+const handleLogin = catchAsync(async (req, res)=>{
+    const login = await userHandlers.logIn(req.body);
+    if (login) res.status(httpStatus.OK).send({ msg: "successful login" });
+    else res.status(httpStatus.BAD_REQUEST).send({ msg: "* Invalid username or password." })
+});
 
-module.exports={handleRegister};
+
+module.exports={handleRegister, handleLogin};
--- src/user/userHandlers.js
+++ src/user/userHandlers.js
@@ -2,17 +2,22 @@ const db = require('../../database/connection');
 
 function userExist(data){
     return db.query('SELECT * FROM users WHERE username=$1', [data.username]).then((result)=>
-    result.rowCount);
+    result.rowCount).catch(err=> err);
 }
 
 function emailExist(data){
     return db.query('SELECT * FROM users WHERE email=$1', [data.email]).then((result)=>
-    result.rowCount);
+    result.rowCount).catch(err=> err);
 }
 
 function register(data){
     return db.query("INSERT INTO users(username, email, password, user_role, join_date) VALUES($1,$2,$3,$4,$5)",
-    [data.username, data.email, data.password, 'user', data.date]).then(res=> res.rowCount);    
+    [data.username, data.email, data.password, 'user', data.date]).then(res=> res.rowCount).catch(err=> err);   
 }
 
-module.exports = {register, userExist, emailExist};
+const logIn = (data)=>{
+    return db.query("SELECT * FROM users WHERE username=$1 AND password=$2", [data.username, data.password])
+    .then(result=> result.rowCount > 0).catch(err=> err);
+};
+
+module.exports = {register, userExist, emailExist, logIn};
